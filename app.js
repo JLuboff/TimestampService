104e300cc249a80ff11b64ba2adef9e491491f7d
Jason Luboff <jluboff@gmail.com>
2017-03-30 21:09:38 +0000

    Update

--- app.js
+++ app.js
@@ -1,8 +1,23 @@
 var express = require("express");
+var moment = require("moment");
+var timeObj = { unix: "", natural: "" };
 var app = express();
 
-app.get("/", function(req, res){
-    res.send("Hello World!");
+app.get("/:time", function(req, res){
+    "use strict";
+    let query = JSON.parse(req.params.time);
+    console.log(typeof JSON.parse(query));
+    console.log(Number.isInteger(query));
+    if(query > moment()){
+        timeObj.unix = null;
+        timeObj.natural = null;
+        res.send(timeObj);
+    } else if (Number.isInteger(query)){
+    timeObj.unix = query;
+    timeObj.natural = moment(query).format("MMMM D, YYYY");
+    res.send(timeObj);
+    }
+   // res.send(moment());
 });
 
 app.listen(8080, function(){
