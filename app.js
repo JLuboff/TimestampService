9e0138dc5ebca799ecde86ff1e950bef1e1be09a
Jason Luboff <jluboff@gmail.com>
2017-03-30 22:04:45 +0000

    Fixed syntax error

--- app.js
+++ app.js
@@ -5,17 +5,19 @@ var app = express();
 
 app.get("/:time", function(req, res){
     "use strict";
-    let query = JSON.parse(req.params.time);
-    console.log(typeof JSON.parse(query));
+    let query = req.params.time;
+    console.log(query);
     console.log(Number.isInteger(query));
     if(query > moment()){
         timeObj.unix = null;
         timeObj.natural = null;
         res.send(timeObj);
-    } else if (Number.isInteger(query)){
+    } else if (Number.isInteger(parseInt(query, 10))){
     timeObj.unix = query;
-    timeObj.natural = moment(query).format("MMMM D, YYYY");
+    timeObj.natural = moment(parseInt(query, 10)).format("MMMM D, YYYY");
     res.send(timeObj);
+    } else {
+        res.send("String");
     }
    // res.send(moment());
 });
